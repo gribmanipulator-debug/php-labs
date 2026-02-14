d6518f72fe10056cc87f2ab681386c9306219fef
Діденко <kn242_dkv@student.ztu.edu.ua>
2026-02-14 13:51:54 +0200

    fix: lr1 task1 margins

--- lr1/variants/v5/task2.php
+++ lr1/variants/v5/task2.php
@@ -2,7 +2,7 @@
 /**
  * Завдання 1: Форматований текст
  *
- * Вірш про художника з форматуванням: <b>, <i>, margin-left
+ * Вірш з форматуванням: <b>, <i>, margin-left
  */
 require_once __DIR__ . '/layout.php';
 
@@ -11,9 +11,9 @@ ob_start();
 <div class="poem">
     <?php
     echo "<p style='margin-left: 20px;'>У саду цвітуть <b>вишні</b> білі,</p>";
-    echo "<p style='margin-left: 20px;'>Бджоли гудуть <i>весело</i> навколо,</p>";
-    echo "<p style='margin-left: 20px;'>Пелюстки падають несміливі,</p>";
-    echo "<p style='margin-left: 20px;'>Весна прийшла у наше село.</p>";
+    echo "<p style='margin-left: 35px;'>Бджоли гудуть <i>весело</i> навколо,</p>";
+    echo "<p style='margin-left: 50px;'>Пелюстки падають несміливі,</p>";
+    echo "<p style='margin-left: 70px;'>Весна прийшла у наше село.</p>";
     ?>
 </div>
 <?php
