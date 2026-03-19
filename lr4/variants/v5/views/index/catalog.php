fcdd0e7f99840d88649b782398245b76055ccc96
Діденко <kn242_dkv@student.ztu.edu.ua>
2026-03-19 12:41:45 +0200

    feat: lr4 v5 - add responsive car card images

--- lr4/variants/v5/views/index/catalog.php
+++ lr4/variants/v5/views/index/catalog.php
@@ -3,24 +3,44 @@
     <p class="page-home__subtitle">Оберіть авто для міста, сім'ї або подорожей та скористайтеся сервісами автосалону.</p>
 
     <div class="card-grid">
-        <article class="card">
-            <h3 class="card__title">Toyota Corolla</h3>
-            <p class="card__text">Надійний седан для щоденного використання, економна витрата пального, стартова ціна від 890 000 грн.</p>
+        <article class="card card--car">
+            <div class="car-card__media">
+                <img class="car-card__image" src="images/cars/toyota-corolla.svg" alt="Toyota Corolla" loading="lazy" decoding="async">
+            </div>
+            <div class="car-card__content">
+                <h3 class="card__title">Toyota Corolla</h3>
+                <p class="card__text">Надійний седан для щоденного використання, економна витрата пального, стартова ціна від 890 000 грн.</p>
+            </div>
         </article>
 
-        <article class="card">
-            <h3 class="card__title">Skoda Octavia</h3>
-            <p class="card__text">Практичний ліфтбек з містким багажником, комфорт для сімейних поїздок, стартова ціна від 980 000 грн.</p>
+        <article class="card card--car">
+            <div class="car-card__media">
+                <img class="car-card__image" src="images/cars/skoda-octavia.svg" alt="Skoda Octavia" loading="lazy" decoding="async">
+            </div>
+            <div class="car-card__content">
+                <h3 class="card__title">Skoda Octavia</h3>
+                <p class="card__text">Практичний ліфтбек з містким багажником, комфорт для сімейних поїздок, стартова ціна від 980 000 грн.</p>
+            </div>
         </article>
 
-        <article class="card">
-            <h3 class="card__title">Volkswagen Tiguan</h3>
-            <p class="card__text">Кросовер для міста та траси, підвищений кліренс, розширені системи безпеки, стартова ціна від 1 350 000 грн.</p>
+        <article class="card card--car">
+            <div class="car-card__media">
+                <img class="car-card__image" src="images/cars/volkswagen-tiguan.svg" alt="Volkswagen Tiguan" loading="lazy" decoding="async">
+            </div>
+            <div class="car-card__content">
+                <h3 class="card__title">Volkswagen Tiguan</h3>
+                <p class="card__text">Кросовер для міста та траси, підвищений кліренс, розширені системи безпеки, стартова ціна від 1 350 000 грн.</p>
+            </div>
         </article>
 
-        <article class="card">
-            <h3 class="card__title">Kia Sportage</h3>
-            <p class="card__text">Сучасний кросовер з вигідними комплектаціями, зручний салон та мультимедіа, стартова ціна від 1 180 000 грн.</p>
+        <article class="card card--car">
+            <div class="car-card__media">
+                <img class="car-card__image" src="images/cars/kia-sportage.svg" alt="Kia Sportage" loading="lazy" decoding="async">
+            </div>
+            <div class="car-card__content">
+                <h3 class="card__title">Kia Sportage</h3>
+                <p class="card__text">Сучасний кросовер з вигідними комплектаціями, зручний салон та мультимедіа, стартова ціна від 1 180 000 грн.</p>
+            </div>
         </article>
     </div>
 
